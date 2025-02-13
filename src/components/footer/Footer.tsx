fdf5b79b75c1c30533d7ba2219670c15a5a55687
tarchevskyivan <i.tarchevsky@gmail.com>
2025-02-13 16:33:47 +0300

    Добавил актуальный год

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
+const year = new Date().getFullYear()
+
 const Footer = () => {
 	return (
 		<footer className='footer footer-center p-10 bg-base-200 text-base-content'>
 			<aside>
 				<Link href='/requisites'>Реквизиты</Link>
-				<p>Copyright © 2024</p>
+				<p>Copyright © {year}</p>
 			</aside>
 		</footer>
 	)
