bee1640fee9d9c8ccbdf54c8835f6bf0af2485d0
Иван <i.tarchevsky@gmail.com>
2026-06-17 19:32:11 +0300

    standalone

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+	output: 'standalone',
 	reactStrictMode: true,
 	images: {
-		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: 'https',
